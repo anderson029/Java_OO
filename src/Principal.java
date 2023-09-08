25e98c264fbbfa579b44d46e1db3d289c90f2960
anderson029 <andersonfoliveira@yahoo.com>
2023-09-08 17:45:45 -0300

    aula 3

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,8 @@ import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
@@ -50,5 +52,20 @@ public class Principal {
         episodio.setSerie(lost);
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
+
+        Filme filmeDoAnderson = new Filme();
+        filmeDoAnderson.setDuracaoEmMinutos(200);
+        filmeDoAnderson.setNome("ToyStory");
+        filmeDoAnderson.setAnoDeLancamento(1995);
+        filmeDoAnderson.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDoAnderson);
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+        System.out.println("Tamanho da lista " + listaDeFilmes.size());
+        System.out.println("Tamanho da lista " + listaDeFilmes.get(0).getNome());
+        System.out.println(listaDeFilmes);
+
     }
 }
